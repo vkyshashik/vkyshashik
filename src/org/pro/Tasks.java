27ffb3f638f0b86531e5b49e2111f797cb067817
vkyshashik <vkyshashik2222@gmail.com>
2022-03-27 10:07:24 +0530

    DONE

---
+++ src/org/pro/Tasks.java
@@ -0,0 +1,24 @@
+package org.pro;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Tasks {
+	public static void main(String[] args) {
+		String s = "Welcome To Java";
+		Map<Character, Integer> m = new LinkedHashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (m.containsKey(c)) {
+				Integer y = m.get(c);
+				m.put(c, y+1);
+				
+			}else {
+				m.put(c, 1);
+			}
+			
+		}
+		System.out.println(m);
+	}
+
+}
